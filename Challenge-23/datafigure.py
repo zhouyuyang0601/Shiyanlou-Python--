9bc381f6a838cd4045c9562659a891bf360fa5dc
Dick Johnson <zhouyuyang0601@gmail.com>
2018-03-17 21:03:23 +0800

    Challenge Matplotlib

--- Challenge-23/datafigure.py
+++ Challenge-23/datafigure.py
@@ -3,9 +3,9 @@ from matplotlib import pyplot as plt
 import os
 
 def data_plot():
-    path=os.path.abspath('user_study.json'))
+    path=os.path.abspath('user_study.json')
     data= pd.read_json(path)
-    df=data[['user_id','minutes']].groupbu('userid').sum()
+    df=data[['user_id','minutes']].groupby('user_id').sum()
     fig=plt.figure()
     ax=fig.add_subplot(1,1,1)
     df.plot()
