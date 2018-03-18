39886bf35e8834fd764e35b6d44ab9ce113e7f68
Dick Johnson <zhouyuyang0601@gmail.com>
2018-03-18 17:27:43 +0800

    Challenge 25 Data_Time_Series
    Use Numpy and Pandas to deal with Apple.inc NASDAQ data, resample by quaters.
    Print the 2nd largest volume

---
+++ Challenge-24/analysis.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import os
+from pandas import Series,DataFrame
+
+def quarter_volum()
+    path=os.path.abspath('apple.csv')
+    data=pd.read_csv(path)
+    data['Date']=pd.to_datetime(data['Date'])
+    ts=pd.Series(list(data['Volume'],index= data['Date']))
+    ts_quarter=ts.resample('Q').sum().sort_values(ascending=False)
+    return ts[1]
+
+
+    
