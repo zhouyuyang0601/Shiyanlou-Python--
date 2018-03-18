2c28d7ad23d6f88ec070e24a6b40fff3d8791f32
Dick Johnson <zhouyuyang0601@gmail.com>
2018-03-18 17:36:19 +0800

    很烦

--- Challenge-24/apple_analysis.py
+++ Challenge-24/apple_analysis.py
@@ -7,9 +7,9 @@ def quarter_volume()
     path=os.path.abspath('apple.csv')
     data=pd.read_csv(path)
     data['Date']=pd.to_datetime(data['Date'])
-    ts=pd.Series(list(data['Volume'],index= data['Date']))
+    ts=pd.Series(list(data['Volume']),index= data['Date'])
     ts_quarter=ts.resample('Q').sum().sort_values(ascending=False)
-    return ts[1]
+    return ts_quarter[1]
 
 
     
