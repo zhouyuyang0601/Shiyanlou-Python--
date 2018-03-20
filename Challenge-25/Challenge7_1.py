41d94833fda5a993201dd4cf219212bdae0fd63b
yuyang <zhouyuyang0601@gmail.com>
2018-03-20 17:48:42 +0800

    Data_analyze
    Familiar with Pandas Operation: slice, merge, isna, join, concat, drop, replace, drop_duplicates, set_index ....

--- Challenge-25/Challenge7_1.py
+++ Challenge-25/Challenge7_1.py
@@ -12,12 +12,20 @@ def co2():
     # 读取世界银行气候变化数据集
     df_climate = pd.read_excel("ClimateChange.xlsx", sheet_name=0)
     df_country = pd.read_excel("ClimateChange.xlsx", sheet_name=1)
+    """
     #数据清洗
     #有缺失值的年份数据进行填充，挑战规定使用近邻数据填充
-    test=df_climate[df_climate['Series code'] == "EN.ATM.CO2E.KT"]
+    #挑战不统计原始数据全部缺失的国家，也就是排放量最低的国家对应的排放量数值不会为 0
+    """
+    #读取CO2年份并累加
+    #读取含有二氧化碳数据的年份，并且
+    co2_data=df_climate[df_climate['Series code'] == "EN.ATM.CO2E.KT"].set_index('Country name').iloc[:,5:24].drop_duplicates(keep=False)
+    co2_fill=co2_data.replace({'..':pd.np.NaN}).fillna(method='ffill',axis=1).fillna(method='bfill',axis=1)
+    co2=pd.DataFrame(co2_fill.sum(axis=1),columns=['CO2 sum submission'])
 
-    sum=test.iloc['1990':'2008']
-    test['Country Code','Country name','Series Code','Sum']
+    #根据索引合并country
+    country=df_country.iloc[:,[1,4]].set_index('Country name')
+    pandas.concat([country,co2])
     
 
     # 必须返回最终得到的 DataFrame
