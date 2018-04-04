c7bd79b760d90ef4c288877fe2af469b8a32508c
yuyang <zhouyuyang0601@gmail.com>
2018-04-04 09:02:52 +0800

    challenge29 - Bug Fix
    Bugs fixed

--- Simple_EDU/Challenge-29/simpledu/handlers/user.py
+++ Simple_EDU/Challenge-29/simpledu/handlers/user.py
@@ -3,8 +3,8 @@ from simpledu.models import User
 #通过username查询数据库获得User对象，get_or_404, 获得对象后传入user.html
 user = Blueprint('user', __name__, url_prefix='/user')
 
-@user.route('/')
-def index():
-    User = User.query.get_or_404(username)
-    return render_template('user.html', user=User)
+@user.route('/<username>')
+def user_index(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
 
