87c8b5ef01e3eb4954eccaf9811dbb2543ccd271
yuyang <zhouyuyang0601@gmail.com>
2018-04-18 10:00:37 +0800

    finished
    apply multipage and xml trick in crawling github repo(branch,commit,release)

--- Scrapy/抓取Github仓库的提取、分支和版本/shiyanlou/items.py
+++ Scrapy/抓取Github仓库的提取、分支和版本/shiyanlou/items.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# http://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
@@ -12,3 +5,6 @@ class GithubItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     update_time = scrapy.Field()
+    branches = scrapy.Field()
+    commits = scrapy.Field()
+    releases = scrapy.Field()
